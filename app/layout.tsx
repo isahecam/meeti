23570407bfbbe3ecf02e1587b2ad2ac28b7e031f
Brandon Hernández <isahecam@gmail.com>
2026-04-20 12:05:57 -0600

    fix: change language attribute from English to Spanish in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="es"
       suppressHydrationWarning
       className={cn("antialiased", fontSans.variable, "font-mono", geistMono.variable)}
     >
