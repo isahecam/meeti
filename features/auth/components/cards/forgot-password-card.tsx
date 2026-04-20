7d4a523459d20dcaec5f2098816e75d23969f4c5
Brandon Hernández <isahecam@gmail.com>
2026-04-20 16:05:08 -0600

    feat: enhance ForgotPasswordCard with navigation links to login and account creation

--- features/auth/components/cards/forgot-password-card.tsx
+++ features/auth/components/cards/forgot-password-card.tsx
@@ -1,18 +1,28 @@
+import Link from "next/link"
+
 import { ForgotPasswordForm } from "~/features/auth/components/forgot-password-form"
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "~/shared/components/ui/card"
+import { buttonVariants } from "~/shared/components/ui/button"
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/shared/components/ui/card"
 
 export function ForgotPasswordCard() {
   return (
     <Card className="mx-auto w-full max-w-sm">
-      <CardHeader className="text-center">
+      <CardHeader>
         <CardTitle>Recuperar Contraseña</CardTitle>
-        <CardDescription className="text-xs">
-          Ingresa tu correo electrónico para recuperar tu acceso a Meeti
-        </CardDescription>
+        <CardDescription className="text-xs">Recupera tu acceso a Meeti</CardDescription>
       </CardHeader>
       <CardContent>
         <ForgotPasswordForm />
       </CardContent>
+      <CardFooter className="justify-between">
+        <Link className={buttonVariants({ variant: "link" })} href="/auth/login">
+          Iniciar Sesión
+        </Link>
+
+        <Link className={buttonVariants({ variant: "link" })} href="/auth/create-account">
+          Crear Cuenta
+        </Link>
+      </CardFooter>
     </Card>
   )
 }
