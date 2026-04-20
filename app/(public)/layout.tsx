45c46f879a31170bf168b70ef0ff5945e54a5ea3
Brandon Hernández <isahecam@gmail.com>
2026-04-20 14:11:45 -0600

    feat: enhance PublicLayout by adding Header component for improved layout structure

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,7 +1,14 @@
+import { Header } from "~/shared/components/layout/header"
+
 export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return <>{children}</>
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  )
 }
