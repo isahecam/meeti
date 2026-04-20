02beb134d343a39cef494ad5018809f24f02ebe9
Brandon Hernández <isahecam@gmail.com>
2026-04-20 15:38:40 -0600

    feat: integrate LoginCard component into login page for streamlined user authentication

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,13 +1,12 @@
 import { Metadata } from "next"
 
+import { LoginCard } from "~/features/auth/components/cards/login-card"
+
 export const metadata: Metadata = {
   title: "Iniciar Sesión",
+  description: "Inicia sesión y empieza a compartir lo que más te gusta con Meeti",
 }
 
 export default function LoginPage() {
-  return (
-    <>
-      <h1>Iniciar Sesión</h1>
-    </>
-  )
+  return <LoginCard />
 }
