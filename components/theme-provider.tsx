d9fe2e8e9eca0fc72813a809201d81fef9255b02
Brandon Hernández <isahecam@gmail.com>
2026-04-20 12:55:58 -0600

    chore: update .oxfmtrc.json print width, format tsconfig.json for consistency, and simplify ThemeProvider component structure

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -5,12 +5,7 @@ import * as React from "react"
 
 function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      {...props}>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange {...props}>
       <ThemeHotkey />
       {children}
     </NextThemesProvider>
