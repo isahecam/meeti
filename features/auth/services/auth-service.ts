4bbe4cadd82094a5df4814cf390bd43f36b16bc1
Brandon Hernández <isahecam@gmail.com>
2026-04-20 21:42:03 -0600

    feat: implement user registration logic in AuthService with improved error handling

--- features/auth/services/auth-service.ts
+++ features/auth/services/auth-service.ts
@@ -1,9 +1,32 @@
+import { APIError } from "better-auth"
+
 import { SignUpType } from "~/features/auth/schemas/auth-schema"
+import { auth } from "~/lib/auth"
+import { err, ok } from "~/lib/result"
 
 class AuthService {
   async register({ name, email, password }: SignUpType) {
     // ! Revisar si el email ya está registrado
+    // ! Validación de negocio
     // ! Crear el usuario en la base de datos
+
+    try {
+      return ok(
+        await auth.api.signUpEmail({
+          body: {
+            name,
+            email,
+            password,
+          },
+        }),
+      )
+    } catch (error: unknown) {
+      if (error instanceof APIError) {
+        return err({ reason: `${error.status}` })
+      }
+
+      return err({ reason: "UNEXPECTED_ERROR" })
+    }
   }
 }
 
