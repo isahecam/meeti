cbf3401797f11b62b782a261b179613d007163cb
Brandon Hernández <isahecam@gmail.com>
2026-04-21 14:13:51 -0600

    feat: add EmailService class for sending emails with React content using the resend API

---
+++ shared/emails/services/email-service.ts
@@ -0,0 +1,22 @@
+import React from "react"
+
+import { resend } from "~/lib/resend"
+import { err, ok } from "~/lib/result"
+
+interface SendEmailOptions {
+  from: string
+  to: string
+  subject: string
+  react: React.ReactNode
+  text: string
+}
+
+export class EmailService {
+  static async send({ from, to, subject, react, text }: SendEmailOptions) {
+    const { data, error } = await resend.emails.send({ from, to, subject, react, text })
+
+    if (error) return err({ reason: error.message })
+
+    return ok(data)
+  }
+}
