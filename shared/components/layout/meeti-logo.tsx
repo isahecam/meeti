a70ea0aa28d8752b60109bf8608f5720aab0f900
Brandon Hernández <isahecam@gmail.com>
2026-04-21 16:20:48 -0600

    feat: enhance MeetiLogo component to accept additional props for customization

--- shared/components/layout/meeti-logo.tsx
+++ shared/components/layout/meeti-logo.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image"
 
-export function MeetiLogo() {
-  return <Image src="/meeti-logo.svg" alt="Meeti Logo" width={120} height={60} className="aspect-auto" />
+export function MeetiLogo({ ...props }: Pick<React.ComponentProps<typeof Image>, "className" | "width" | "height">) {
+  return <Image src="/meeti-logo.svg" alt="Meeti Logo" width={120} height={60} className="aspect-auto" {...props} />
 }
