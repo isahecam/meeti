da9220ccc14f010adf6ea23cf6315fcaa6588bd3
Brandon Hernández <isahecam@gmail.com>
2026-04-21 16:18:16 -0600

    feat: add NAV_MAIN_ITEMS constant for main navigation items in the dashboard

---
+++ features/dashboard/constants/nav-main-items.ts
@@ -0,0 +1,26 @@
+import { Bell, Folder, Home, Users } from "lucide-react"
+
+import { NavItem } from "~/features/dashboard/types/dashboard-types"
+
+export const NAV_MAIN_ITEMS = [
+  {
+    title: "Panel de Administración",
+    url: "/dashboard",
+    icon: Home,
+  },
+  {
+    title: "Comunidades",
+    url: "#",
+    icon: Users,
+  },
+  {
+    title: "Meetis",
+    url: "#",
+    icon: Folder,
+  },
+  {
+    title: "Notificaciones",
+    url: "#",
+    icon: Bell,
+  },
+] satisfies NavItem[]
