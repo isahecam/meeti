85a4ce209c36ac4b0aea4fb329811c7a7ee30215
Brandon Hernández <isahecam@gmail.com>
2026-04-21 11:46:22 -0600

    feat: implement user existence check in AuthRepository for improved authentication logic

--- features/auth/services/auth-respository.ts
+++ features/auth/services/auth-respository.ts
@@ -1,3 +1,17 @@
-class AuthRepository {}
+import { User } from "better-auth"
+
+import { db } from "~/db"
+
+export interface IAuthRepository {
+  userExists(email: string): Promise<User | undefined>
+}
+
+class AuthRepository implements IAuthRepository {
+  async userExists(email: string) {
+    return await db.query.users.findFirst({
+      where: (users, { eq }) => eq(users.email, email),
+    })
+  }
+}
 
 export const authRepository = new AuthRepository()
