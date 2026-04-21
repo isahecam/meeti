3136958e8fe5a23910be56ae719decd65a9ecb53
Brandon Hernández <isahecam@gmail.com>
2026-04-21 13:07:47 -0600

    feat: enhance LoginForm with controlled inputs, validation, and loading state for improved user experience

--- features/auth/components/login-form.tsx
+++ features/auth/components/login-form.tsx
@@ -1,36 +1,107 @@
 "use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
 import Link from "next/link"
+import { useTransition } from "react"
+import { Controller, useForm } from "react-hook-form"
+import { toast } from "sonner"
 
+import { signInAction } from "~/features/auth/actions/sign-in-action"
+import { getAuthErrorMessage } from "~/features/auth/lib/get-auth-error-message"
+import { SignInSchema, SignInType } from "~/features/auth/schemas/auth-schema"
 import { Form } from "~/shared/components/forms/form"
 import { FormSubmitButton } from "~/shared/components/forms/form-submit-button"
 import { buttonVariants } from "~/shared/components/ui/button"
-import { Field, FieldContent, FieldGroup, FieldLabel } from "~/shared/components/ui/field"
+import { Field, FieldContent, FieldError, FieldGroup, FieldLabel } from "~/shared/components/ui/field"
 import { Input } from "~/shared/components/ui/input"
+import { Spinner } from "~/shared/components/ui/spinner"
 
 export function LoginForm() {
-  return (
-    <Form>
-      <FieldGroup>
-        <Field>
-          <FieldLabel htmlFor="name">Correo Electrónico</FieldLabel>
-          <Input id="name" type="email" autoComplete="email" placeholder="m@example.com" />
-        </Field>
+  const [isPending, startTransition] = useTransition()
 
-        <Field>
-          <FieldContent className="flex-row">
-            <FieldLabel htmlFor="password">Contraseña</FieldLabel>
-            <Link
-              href="/auth/forgot-password"
-              className={buttonVariants({ variant: "link", className: "ml-auto px-0!" })}>
-              ¿Olvidaste tu contraseña?
-            </Link>
-          </FieldContent>
-          <Input id="password" type="password" placeholder="********" />
-        </Field>
+  const { control, handleSubmit, reset } = useForm<SignInType>({
+    resolver: zodResolver(SignInSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "all",
+  })
+
+  const onSubmit = (data: SignInType) => {
+    startTransition(async () => {
+      const [error] = await signInAction(data)
+
+      if (error) {
+        toast.error(getAuthErrorMessage(error.reason))
+        return
+      }
+
+      toast.success("Tu sesión ha sido iniciada correctamente")
+
+      reset()
+    })
+  }
+
+  return (
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FieldGroup>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor="email">Correo Electrónico</FieldLabel>
+              <Input
+                {...field}
+                aria-invalid={fieldState.invalid}
+                id="email"
+                type="email"
+                autoComplete="email"
+                placeholder="tu@email.com"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+
+        <Controller
+          name="password"
+          control={control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldContent className="flex-row">
+                <FieldLabel htmlFor="password">Contraseña</FieldLabel>
+                <Link
+                  href="/auth/forgot-password"
+                  className={buttonVariants({ variant: "link", className: "ml-auto h-auto p-0!" })}>
+                  ¿Olvidaste tu contraseña?
+                </Link>
+              </FieldContent>
+              <Input
+                {...field}
+                aria-invalid={fieldState.invalid}
+                id="password"
+                type="password"
+                autoComplete="current-password"
+                placeholder="********"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
       </FieldGroup>
 
-      <FormSubmitButton className="w-full">Iniciar Sesión</FormSubmitButton>
+      <FormSubmitButton className="w-full" disabled={isPending}>
+        {isPending ? (
+          <>
+            <Spinner data-icon="inline-start" />
+            Iniciando sesión...
+          </>
+        ) : (
+          "Iniciar Sesión"
+        )}
+      </FormSubmitButton>
     </Form>
   )
 }
