e0d3bb75cc7aa911a39ed1d68b4181574dea31e6
Brandon Hernández <isahecam@gmail.com>
2026-04-20 18:27:21 -0600

    feat: implement sign-up action to handle user registration with validation

---
+++ features/auth/actions/sign-up-action.ts
@@ -0,0 +1,13 @@
+"use server"
+
+import { SignUpSchema, SignUpType } from "~/features/auth/schemas/auth-schema"
+import { authService } from "~/features/auth/services/auth-service"
+import { err } from "~/lib/result"
+
+export async function signUpAction(values: SignUpType) {
+  const { success, data } = SignUpSchema.safeParse(values)
+
+  if (!success) return err({ reason: "invalid_data" })
+
+  await authService.register(data)
+}
