b49687ad9c383a3a08ac895abfbccbeee75c6fd7
Brandon Hernández <isahecam@gmail.com>
2026-04-21 20:26:30 -0600

    fix: adjust layout of SiteHeader for improved alignment and spacing

--- features/dashboard/components/site-header.tsx
+++ features/dashboard/components/site-header.tsx
@@ -7,9 +7,9 @@ import { SidebarTrigger } from "~/shared/components/ui/sidebar"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+      <div className="flex h-5 w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
-        <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+        <Separator orientation="vertical" className="mr-2" />
         <Breadcrumbs />
       </div>
 
