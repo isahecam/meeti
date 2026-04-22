616d2c75bb3943e44a7ae84e9ca98b3d42471dc8
Brandon Hernández <isahecam@gmail.com>
2026-04-21 19:43:47 -0600

    fix: remove extra space in heading component sizeMap for level 1

--- shared/components/typography/heading.tsx
+++ shared/components/typography/heading.tsx
@@ -9,7 +9,7 @@ export function Heading({ children, level = 1, className, ...props }: Props) {
   const Tag: React.ElementType = `h${level}`
 
   const sizeMap = {
-    1: "scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance",
+    1: "scroll-m-20  text-4xl font-extrabold tracking-tight text-balance",
     2: "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
     3: "scroll-m-20 text-2xl font-semibold tracking-tight",
     4: "scroll-m-20 text-xl font-semibold tracking-tight",
