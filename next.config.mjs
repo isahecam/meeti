da8e93894aede10d88e8448c3e8e0285ac3cafc1
Brandon Hernández <isahecam@gmail.com>
2026-04-22 14:03:18 -0600

    feat: add remote image pattern for external image hosting in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typedRoutes: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "5nxxrksz1w.ufs.sh",
+      },
+    ],
+  },
 }
 
 export default nextConfig
