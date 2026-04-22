3547da8e82ff6d7372a10eaeedcfe9d9391bb07e
Brandon Hernández <isahecam@gmail.com>
2026-04-22 16:53:40 -0600

    feat: add CommunityContext and CommunityPermissions interfaces to manage community membership and permissions

--- features/communities/types/community-types.ts
+++ features/communities/types/community-types.ts
@@ -2,3 +2,22 @@ import { communitiesTable } from "~/db/schema/communities"
 
 export type InsertCommunity = typeof communitiesTable.$inferInsert
 export type SelectCommunity = typeof communitiesTable.$inferSelect
+
+export interface CommunityContext {
+  isMember: boolean
+  isAdmin: boolean
+}
+
+export interface CommunityPermissions {
+  canEdit: boolean
+  canDelete: boolean
+  canJoin: boolean
+  canLeave: boolean
+  canViewMembers: boolean
+}
+
+export interface CommunityWithPermissions {
+  data: SelectCommunity
+  context: CommunityContext
+  permissions: CommunityPermissions
+}
