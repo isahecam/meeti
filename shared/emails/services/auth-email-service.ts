fb09e4f75b01a3150ec08b90d240196a25b5f899
Brandon Hernández <isahecam@gmail.com>
2026-04-21 18:37:35 -0600

    feat: implement sendPasswordResetEmail method in AuthEmailService for password recovery

--- shared/emails/services/auth-email-service.ts
+++ shared/emails/services/auth-email-service.ts
@@ -1,5 +1,6 @@
 import { emailConfig } from "~/shared/emails/config"
 import { EmailService } from "~/shared/emails/services/email-service"
+import { PasswordResetEmail, renderPasswordResetEmailText } from "~/shared/emails/templates/password-reset-email"
 import { renderVerificationEmailText, VerificationEmail } from "~/shared/emails/templates/verification-email"
 
 export class AuthEmailService {
@@ -13,8 +14,20 @@ export class AuthEmailService {
     await EmailService.send({
       from: emailConfig.from.verification,
       to: email,
-      subject: "Verificación de cuenta",
+      subject: "Meeti - Verificación de cuenta",
       react: VerificationEmail({ name, url, tokenExpiration: emailConfig.tokenExpiration }),
+      text,
+    })
+  }
+
+  static async sendPasswordResetEmail(name: string, email: string, url: string) {
+    const { text } = await renderPasswordResetEmailText({ name, url })
+
+    await EmailService.send({
+      from: emailConfig.from.passwordReset,
+      to: email,
+      subject: "Meeti - Recuperación de contraseña",
+      react: PasswordResetEmail({ name, url }),
       text,
     })
   }
