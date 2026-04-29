e88f67edd8922849ee9b305e1d36507c1113308e
Brandon Hernández <isahecam@gmail.com>
2026-04-29 16:41:49 -0600

    feat: add findById and update methods to CommunityRepository for retrieving and modifying community details

--- features/communities/services/community-repository.ts
+++ features/communities/services/community-repository.ts
@@ -3,11 +3,14 @@ import { eq } from "drizzle-orm"
 
 import { db } from "~/db"
 import { communitiesTable } from "~/db/schema"
+import { CommunityType } from "~/features/communities/schemas/community-schema"
 import { InsertCommunity, SelectCommunity } from "~/features/communities/types/community-types"
 
 export interface ICommunityRepository {
   create(data: InsertCommunity): Promise<SelectCommunity>
   findByUserId(userId: User["id"], limit?: number): Promise<SelectCommunity[]>
+  findById(id: SelectCommunity["id"]): Promise<SelectCommunity | undefined>
+  update(communityId: SelectCommunity["id"], data: CommunityType): Promise<SelectCommunity | undefined>
 }
 
 class CommunityRepository implements ICommunityRepository {
@@ -23,6 +26,17 @@ class CommunityRepository implements ICommunityRepository {
       .where(eq(communitiesTable.createdBy, userId))
       .limit(limit)
     return communities
+  }
+
+  async findById(communityId: SelectCommunity["id"]) {
+    const [result] = await db.select().from(communitiesTable).where(eq(communitiesTable.id, communityId)).limit(1)
+    return result
+  }
+
+  async update(communityId: SelectCommunity["id"], data: CommunityType) {
+    const [result] = await db.update(communitiesTable).set(data).where(eq(communitiesTable.id, communityId)).returning()
+
+    return result
   }
 }
 
