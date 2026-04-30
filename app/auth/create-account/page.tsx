0ead14892ff49b7bfec7e577f6d1f8b4f39d54fd
Brandon Hernández <isahecam@gmail.com>
2026-04-29 20:21:25 -0600

    feat: update authentication flow in Register and Login pages to redirect users with active sessions to the dashboard

--- app/auth/create-account/page.tsx
+++ app/auth/create-account/page.tsx
@@ -1,12 +1,20 @@
 import { Metadata } from "next"
+import { redirect } from "next/navigation"
 
 import { RegisterCard } from "~/features/auth/components/cards/register-card"
+import { requireAuth } from "~/lib/auth-server"
 
 export const metadata: Metadata = {
   title: "Crear Cuenta",
   description: "Crea una cuenta para empezar a usar Meeti",
 }
 
-export default function RegisterPage() {
+export default async function RegisterPage() {
+  const { session } = await requireAuth()
+
+  if (session) {
+    redirect("/dashboard")
+  }
+
   return <RegisterCard />
 }
--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,19 @@
 import { Metadata } from "next"
+import { redirect } from "next/navigation"
 
 import { LoginCard } from "~/features/auth/components/cards/login-card"
+import { requireAuth } from "~/lib/auth-server"
 
 export const metadata: Metadata = {
   title: "Iniciar Sesión",
   description: "Inicia sesión y empieza a compartir lo que más te gusta con Meeti",
 }
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const { session } = await requireAuth()
+  if (session) {
+    redirect("/dashboard")
+  }
+
   return <LoginCard />
 }
