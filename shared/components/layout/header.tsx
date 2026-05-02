d1243726cd378d4e8fdee033ae5527f9d1da478e
Brandon Hernández <isahecam@gmail.com>
2026-05-02 16:27:55 -0600

    feat: update Header component to include user session handling and conditional navigation

--- shared/components/layout/header.tsx
+++ shared/components/layout/header.tsx
@@ -1,9 +1,12 @@
 import Link from "next/link"
 
+import { getServerSession } from "~/lib/auth-server"
 import { GuestNavigation } from "~/shared/components/layout/guest-navigation"
 import { MeetiLogo } from "~/shared/components/layout/meeti-logo"
+import { UserNavigation } from "~/shared/components/layout/user-navigation"
 
-export function Header() {
+export async function Header() {
+  const session = await getServerSession()
   return (
     <header>
       <div className="mx-auto max-w-7xl p-4 md:flex md:items-center md:justify-between lg:px-0">
@@ -12,7 +15,7 @@ export function Header() {
             <MeetiLogo />
           </Link>
         </div>
-        <GuestNavigation />
+        {session ? <UserNavigation user={session.user} /> : <GuestNavigation />}
       </div>
     </header>
   )
