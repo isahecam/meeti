f9f3547ca453c7af228ef019a588d523929a5dcf
Brandon Hernández <isahecam@gmail.com>
2026-05-02 16:30:21 -0600

    fix: adjust grid layout in UserCommunitiesList component for improved responsiveness

--- features/communities/components/layout/user-communities-list.tsx
+++ features/communities/components/layout/user-communities-list.tsx
@@ -21,7 +21,7 @@ export async function UserCommunitiesList() {
 
   return (
     <>
-      <ul className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <ul className="grid w-full grid-cols-1 gap-4 md:grid-cols-2">
         {communities.map((community) => (
           <li key={community.data.id}>
             <UserCommunityCard community={community} />
